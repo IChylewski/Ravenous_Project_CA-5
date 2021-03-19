93908f6821730c5ded8b306b7c6f7f46c1a5c181
IChylewski <ireneuszchylewski@gmail.com>
2021-03-19 11:11:50 +0000

    Yelp Api and Google Maps finished

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Business.css';
-
+import  Maps  from '../../util/GoogleMaps'
 
 
 class Business extends React.Component {
@@ -13,9 +13,11 @@ class Business extends React.Component {
                 <h2>{this.props.business.name}</h2>
                 <div className='Business-information'>
                     <div className='Business-address'>
+                        <a target='_blank' rel='noreferrer' href={Maps.search(this.props.business.address, this.props.business.city, this.props.business.zipCode)}>
                         <p>{this.props.business.address}</p>
                         <p>{this.props.business.city}</p>
                         <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        </a>    
                     </div>
                     <div className='Business=reviews'>
                         <h3>{this.props.business.category}</h3>
